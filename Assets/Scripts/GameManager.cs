15cda443a517249f0f9ea30b439ed773b76f9f0d
Livecan <roman.meszaros@gmail.com>
2021-10-21 08:49:50 +0200

    Moving scenario.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,78 @@
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private List<Piece> playerPieces;
-    [SerializeField] private List<Piece> opponentPieces;
+    [SerializeField] private List<Piece> playerPiecesPrefabs;
+    [SerializeField] private List<Piece> opponentPiecesPrefabs;
+
+    private List<Piece> m_playerActivePieces = new List<Piece>();
+    private List<Piece> m_opponentActivePieces = new List<Piece>();
+
+    private bool playerTurn = true;
+    private Turn[] turns;
+    private int turnIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerPiecesPrefabs.ForEach(piece => piece.OnFinishedMove.AddListener(NextTurn));
+        opponentPiecesPrefabs.ForEach(piece => piece.OnFinishedMove.AddListener(NextTurn));
+
         for (int i = 0; i < 3; i++)
         {
-            playerPieces[0].startPosition = new Piece.Position(0, i + 1);
-            opponentPieces[0].startPosition = new Piece.Position(8, i + 1);
+            Piece playerPiece = Instantiate(playerPiecesPrefabs[i]);
+            playerPiece.StartPosition = new Piece.Position(0, i + 1);
+            m_playerActivePieces.Add(playerPiece);
+            m_playerActivePieces[i].OnFinishedMove.AddListener(NextTurn);
+
+            Piece opponentPiece = Instantiate(opponentPiecesPrefabs[i]);
+            opponentPiece.StartPosition = new Piece.Position(8, i + 1);
+            m_opponentActivePieces.Add(opponentPiece);
+            m_opponentActivePieces[i].OnFinishedMove.AddListener(NextTurn);
         }
+
+        turns = new Turn[]
+        {
+            new Turn(m_playerActivePieces[0], new Piece.Position(1, 1)),
+            new Turn(m_opponentActivePieces[1], new Piece.Position(5, 2)),
+            new Turn(m_playerActivePieces[1], new Piece.Position(3, 2)),
+            new Turn(m_opponentActivePieces[2], new Piece.Position(6, 1)),
+            new Turn(m_playerActivePieces[0], new Piece.Position(2, 1)),
+            new Turn(m_opponentActivePieces[2], new Piece.Position(3, 1)),
+            new Turn(m_playerActivePieces[2], new Piece.Position(3, 3)),
+            new Turn(m_opponentActivePieces[0], new Piece.Position(7, 1)),
+        };
+
+        NextTurn();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void NextTurn()
+    {
+        if (turnIndex < turns.Length)
+        {
+            turns[turnIndex].Piece.GoTo(turns[turnIndex].Position);
+            turnIndex++;
+        }
+    }
+
+    public class Turn
+    {
+        public Piece Piece;
+        public Piece.Position Position;
+
+        public Turn(Piece piece, Piece.Position position)
+        {
+            this.Piece = piece;
+            this.Position = position;
+        }
     }
 }
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -5,7 +5,22 @@ using UnityEngine.Events;
 
 public class Piece : MonoBehaviour
 {
-    public Position startPosition;
+    private Position startPosition;
+    public Position StartPosition
+    {
+        set
+        {
+            if (startPosition == null)
+            {
+                startPosition = value;
+                transform.position = value.ToVector3(transform.position.y);
+            }
+            else
+            {
+                throw new System.InvalidOperationException("Start position already assigned!");
+            }
+        }
+    }
 
     private Position m_currentPosition;
     private Position m_targetPosition;
@@ -39,9 +54,6 @@ public class Piece : MonoBehaviour
             Vector3 targetPosition = m_targetPosition.ToVector3();
             targetPosition.y = transform.position.y;
 
-            Debug.Log(targetPosition);
-
-            Debug.Log((targetPosition - transform.position).magnitude + ", " + moveDistance);
             // when the piece is closer to the target position than the last move distance, it's reached the position
             if ((targetPosition - transform.position).magnitude < moveDistance)
             {
@@ -51,23 +63,23 @@ public class Piece : MonoBehaviour
                 // if planned attack, do it before the end of the turn
                 if (m_attackPiece)
                 {
-                    throw new System.NotImplementedException("Attack not implemented.");
+                    throw new System.NotImplementedException();
                     // Attack();
                 }
-
+                Debug.Log("Finished move.");
                 OnFinishedMove.Invoke();
             }
 
         }
     }
 
-    void GoTo(Position position)
+    public void GoTo(Position position)
     {
         m_targetPosition = position;
         m_attackPiece = null;
     }
 
-    void GoTo(Piece opponent)
+    public void GoTo(Piece opponent)
     {
         m_targetPosition = GetPositionBeforeAttack(m_targetPosition, opponent.Pos);
         m_attackPiece = opponent;
@@ -76,7 +88,7 @@ public class Piece : MonoBehaviour
     Position GetPositionBeforeAttack(Position initialPosition, Position targetPosition)
     {
         Position positionBeforeAttack;// TODO: do the magic
-        throw new System.NotImplementedException("Get position before attack not yet implemented.");
+        throw new System.NotImplementedException();
         return positionBeforeAttack;
     }
 
@@ -88,9 +100,9 @@ public class Piece : MonoBehaviour
         private float rowVectorAdjustment = -4;
         private float columnVectorAdjustment = -2;
 
-        public Vector3 ToVector3()
+        public Vector3 ToVector3(float y = 0)
         {
-            return new Vector3(Row + rowVectorAdjustment, 0, Column + columnVectorAdjustment);
+            return new Vector3(Row + rowVectorAdjustment, y, Column + columnVectorAdjustment);
         }
 
         public Position(int row, int column)
