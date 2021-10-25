acd95dd51a1ed2fb2b93567a160226cb83c68f0c
Livecan <roman.meszaros@gmail.com>
2021-10-25 11:56:58 +0200

    GetAvailablePositions for Queen

---
+++ Assets/Scripts/Queen.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Queen : Piece
+{
+    private int moveLength = 3;
+    private GameManager gameManager;
+
+    public void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    public override List<Position> GetAvailablePositions()
+    {
+        List<Piece> allPieces = new List<Piece>(FindObjectsOfType<Piece>());
+        List<Position> availablePositions = new List<Position>();
+
+        availablePositions.AddRange(GetAvailablePositions(gameManager, CurrentPosition, allPieces, new Position(1, 0), moveLength));
+        availablePositions.AddRange(GetAvailablePositions(gameManager, CurrentPosition, allPieces, new Position(1, 1), moveLength));
+        availablePositions.AddRange(GetAvailablePositions(gameManager, CurrentPosition, allPieces, new Position(0, 1), moveLength));
+        availablePositions.AddRange(GetAvailablePositions(gameManager, CurrentPosition, allPieces, new Position(-1, 1), moveLength));
+        availablePositions.AddRange(GetAvailablePositions(gameManager, CurrentPosition, allPieces, new Position(-1, 0), moveLength));
+        availablePositions.AddRange(GetAvailablePositions(gameManager, CurrentPosition, allPieces, new Position(-1, -1), moveLength));
+        availablePositions.AddRange(GetAvailablePositions(gameManager, CurrentPosition, allPieces, new Position(0, -1), moveLength));
+        availablePositions.AddRange(GetAvailablePositions(gameManager, CurrentPosition, allPieces, new Position(1, -1), moveLength));
+
+        return availablePositions;
+    }
+}
